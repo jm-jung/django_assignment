340dd51049f5159f63ac08497265f361776e8831
jm-jung <jmsk7554@gmail.com>
2024-08-27 00:18:53 +0900

    chapter_03 task complete

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
-
+from login_auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todos/', include('todo.urls')),
+    path('', include('todo.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/',auth_views.signup, name='signup'),
+
 ]
