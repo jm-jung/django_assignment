84d5063e3c8592aac02cf1916d8e9b45e1ede40a
jm-jung <jmsk7554@gmail.com>
2024-09-02 15:11:04 +0900

    chapter_06 task

--- todo/forms.py
+++ todo/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from todo.models import Todo
+from todo.models import Todo, Comment
 
 
 class TodoForm(forms.ModelForm):
@@ -13,3 +13,15 @@ class TodoUpdateForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = ["title", "description", "start_date", "end_date", "is_completed"]
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('message',)
+        widgets={
+            'message':forms.Textarea(attrs={'class':'form-control','placeholder':'댓글을 작성하세요','rows':10,'cols':20}),
+        }
+        labels={
+            'message':'내용'
+        }
--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-from .cb_views import TodoListView,TodoDetailView,TodoCreateView,TodoUpdateView,TodoDeleteView
+from .cb_views import TodoListView, TodoDetailView, TodoCreateView, TodoUpdateView, TodoDeleteView, CommentCreateView, \
+    CommentUpdateView, CommentDeleteView
 
 app_name = 'todo'
 urlpatterns = [
@@ -10,9 +11,12 @@ urlpatterns = [
     # path('<int:todo_id>/update', views.todo_update, name='todo_update'),
     # path('<int:todo_id>/delete', views.todo_delete, name='todo_delete'),
 
-path('', TodoListView.as_view(), name='list'),
+    path('', TodoListView.as_view(), name='list'),
     path('<int:todo_pk>/', TodoDetailView.as_view(), name='info'),
     path('create/',TodoCreateView.as_view(), name='create'),
-    path('<int:todo_pk>/update', TodoUpdateView.as_view(), name='update'),
-    path('<int:todo_pk>/delete', TodoDeleteView.as_view(), name='delete'),
+    path('<int:pk>/update/', TodoUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', TodoDeleteView.as_view(), name='delete'),
+    path('comment/create/<int:todo_pk>/',CommentCreateView.as_view(), name='comment_create'),
+    path('comment/update/<int:pk>/',CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/delete/<int:pk>/',CommentDeleteView.as_view(), name='comment_delete'),
 ]
